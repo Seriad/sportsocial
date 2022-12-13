0367ed37f36e7239658a4105f79f833ee163677f
Romain <rspeck52@gmail.com>
2022-12-13 12:12:50 +0100

    Ajout de sport et d'évènement ainsi que d'un event et sport repository

---
+++ SportSocial/src/main/java/fr/solutec/repository/SportRepository.java
@@ -0,0 +1,10 @@
+package fr.solutec.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import fr.solutec.entities.Sport;
+
+
+public interface SportRepository extends CrudRepository<Sport, Long>{
+
+}
