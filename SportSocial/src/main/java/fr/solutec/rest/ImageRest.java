c9d41b4880806d2853264a41c0764c8b3fc42f25
siwertzm <siwertzm@gmail.com>
2022-12-13 14:23:43 +0100

    Creatione ImageRest

---
+++ SportSocial/src/main/java/fr/solutec/rest/ImageRest.java
@@ -0,0 +1,23 @@
+package fr.solutec.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import fr.solutec.entities.Image;
+import fr.solutec.repository.ImageRepository;
+
+@RestController
+@CrossOrigin("*")
+public class ImageRest {
+	
+	@Autowired
+	private ImageRepository imageRepo;
+	
+	@GetMapping("image")
+	public Iterable<Image> getAllImage() {
+		return imageRepo.findAll();
+	}
+
+}
