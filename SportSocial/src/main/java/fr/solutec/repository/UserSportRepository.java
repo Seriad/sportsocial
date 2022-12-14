ace07ebda45e18fd445fae44f5c27e5eab03aece
Augustin36000 <laaugustin@esic.fr>
2022-12-14 09:52:54 +0100

    Ajout API recherche Coach avec sport

--- SportSocial/src/main/java/fr/solutec/repository/UserSportRepository.java
+++ SportSocial/src/main/java/fr/solutec/repository/UserSportRepository.java
@@ -4,12 +4,10 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-
-import fr.solutec.entities.User;
 import fr.solutec.entities.UserSport;
 
 public interface UserSportRepository extends CrudRepository<UserSport, Long > {
 	
 	@Query("SELECT u FROM UserSport u WHERE u.sportFK.nameSport = ?1 AND u.userFK.coachUser = true")
-	public List<User> searchUserBySport(String nameSport);
+	public List<UserSport> searchUserBySport(String nameSport);
 }
