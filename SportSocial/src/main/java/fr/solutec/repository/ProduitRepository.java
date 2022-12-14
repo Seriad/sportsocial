020f0a9d5a543caeb106dfaf17a6b4f8e3a3d095
Augustin36000 <laaugustin@esic.fr>
2022-12-14 10:35:50 +0100

    Création ProduitRepository + ProduitRest

---
+++ SportSocial/src/main/java/fr/solutec/repository/ProduitRepository.java
@@ -0,0 +1,18 @@
+package fr.solutec.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import fr.solutec.entities.Produit;
+
+public interface ProduitRepository extends CrudRepository<Produit, Long> {
+
+	public Iterable<Produit> findAll();
+	
+	@Query("SELECT p FROM Produit p WHERE descriptionProduit LIKE %?1%")
+	public  List<Produit> getProduitByDescriptif(String description);
+	
+
+}
