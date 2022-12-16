ade139d284b3f89b0f7f87a4eb8262569ec5e956
Romain <rspeck52@gmail.com>
2022-12-16 14:41:17 +0100

    zdzdzd

--- SportSocial/src/main/java/fr/solutec/repository/UserActivityRepository.java
+++ SportSocial/src/main/java/fr/solutec/repository/UserActivityRepository.java
@@ -1,9 +1,12 @@
 package fr.solutec.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import fr.solutec.entities.UserActivity;
 
 public interface UserActivityRepository extends CrudRepository<UserActivity, Long>{
 
+	List<UserActivity>findByUserIdUser(Long iduser);
 }
--- SportSocial/src/main/java/fr/solutec/rest/UserActivityRest.java
+++ SportSocial/src/main/java/fr/solutec/rest/UserActivityRest.java
@@ -1,10 +1,33 @@
 package fr.solutec.rest;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import fr.solutec.entities.UserActivity;
+import fr.solutec.entities.UserEvent;
+import fr.solutec.repository.UserActivityRepository;
+import fr.solutec.repository.UserRepository;
 
 @RestController
 @CrossOrigin("*")
 public class UserActivityRest {
+@Autowired
+private UserActivityRepository userActivityRepository;
+@Autowired
+private UserRepository userRepo;
+
+
+
+@GetMapping("Activity/{iduser}")
+public List<UserActivity> getUserActivity (@PathVariable Long iduser) {
+	return userActivityRepository.findByUserIdUser(iduser);
+}
+
 
 }
