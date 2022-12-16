6fd30b8aad3ad53b55882a98e1057938c3209733
LelucFromEsic <leluc@etudiant-esic.fr>
2022-12-16 17:14:35 +0100

    debut api trouver event des amis

--- SportSocial/src/main/java/fr/solutec/rest/UserEventRest.java
+++ SportSocial/src/main/java/fr/solutec/rest/UserEventRest.java
@@ -1,7 +1,10 @@
 package fr.solutec.rest;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -37,5 +40,11 @@ public class UserEventRest {
 		return userEventRepo.save(userEvent);
 	}
 	
+	/*
+	@GetMapping("event/user/{idUser}")
+	public List<Event> getEventOfUser(@PathVariable Long idUser){
+		
+	}
+	*/
 
 }
