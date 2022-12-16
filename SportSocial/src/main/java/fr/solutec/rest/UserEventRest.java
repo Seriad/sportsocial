d5c44f3756b55d3002786424050833ad407492ed
LelucFromEsic <leluc@etudiant-esic.fr>
2022-12-16 16:10:01 +0100

    Mapping ajouter un event a un user

--- SportSocial/src/main/java/fr/solutec/rest/UserEventRest.java
+++ SportSocial/src/main/java/fr/solutec/rest/UserEventRest.java
@@ -7,9 +7,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import fr.solutec.entities.Event;
+import fr.solutec.entities.User;
 import fr.solutec.entities.UserEvent;
 import fr.solutec.repository.EventRepository;
 import fr.solutec.repository.UserEventRepository;
+import fr.solutec.repository.UserRepository;
 
 @RestController
 @CrossOrigin("*")
@@ -21,11 +24,17 @@ public class UserEventRest {
 	@Autowired
 	private UserEventRepository userEventRepo;
 	
+	@Autowired
+	private UserRepository userRepo;
 	
-	@PostMapping("event/ajouter/{id}")
-	public UserEvent addEventToUser(@PathVariable Long idUser, @RequestBody UserEvent userEvent) {
+	
+	@PostMapping("event/ajouter/{idUser}")
+	public UserEvent addEventToUser(@PathVariable Long idUser, @RequestBody Event event) {
 		
-		return userEvent;
+		User u = userRepo.findById(idUser).get();
+		UserEvent userEvent = new UserEvent(u, event);
+		
+		return userEventRepo.save(userEvent);
 	}
 	
 
