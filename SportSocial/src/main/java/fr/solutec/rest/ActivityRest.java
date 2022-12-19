ce9560dae6e4a69da22da72a6196a79c9c022f2f
JeremyH1998 <jhenriongp@gmail.com>
2022-12-19 10:48:13 +0100

    ajout activité schedule

--- SportSocial/src/main/java/fr/solutec/rest/ActivityRest.java
+++ SportSocial/src/main/java/fr/solutec/rest/ActivityRest.java
@@ -1,12 +1,29 @@
 package fr.solutec.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import fr.solutec.entities.Activity;
+import fr.solutec.repository.ActivityRepository;
+
 
 @RestController
 @CrossOrigin("*")
 public class ActivityRest {
 	
-	//Voir toutes les activités d'un user (par son id) + test commit
+	@Autowired
+	private ActivityRepository activityRepo;
+	
+	
+	
+	
+	@PostMapping("schedule")
+	public Activity saveActivity(@RequestBody Activity a) {
+		return activityRepo.save(a);
+	}
+	
 
 }
