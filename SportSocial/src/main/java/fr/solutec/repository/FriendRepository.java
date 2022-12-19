974c35e61555864a8b34f2e7883399ee4c27317b
Romain <rspeck52@gmail.com>
2022-12-19 13:34:46 +0100

    Merge branch 'master' of https://github.com/Seriad/sportsocial.git

--- SportSocial/src/main/java/fr/solutec/repository/FriendRepository.java
+++ SportSocial/src/main/java/fr/solutec/repository/FriendRepository.java
@@ -20,6 +20,10 @@ public interface FriendRepository extends CrudRepository<Friend, Long> {
     @Query("SELECT f FROM Friend f WHERE (f.applicant.id = ?1 OR f.receiver.id = ?1) AND accept = false OR (f.receiver.id = ?1 OR f.applicant.id = ?1) AND accept = false")
     List<Friend> deleteMyFriends (Long idApplicant, Long idReceiver);
     
-    
+    @Query("UPDATE Friend f SET f.accept=true WHERE (f.applicant.id = ?1 OR f.receiver.id = ?1) AND accept = false OR (f.receiver.id = ?1 OR f.applicant.id = ?1) AND accept = false")
+    List<Friend> updateMyFriends (Long idApplicant, Long idReceiver);
 
+
+    @Query("SELECT f FROM Friend f WHERE (f.applicant.id = ?1 OR f.receiver.id = ?1) AND accept = false OR (f.receiver.id = ?1 OR f.applicant.id = ?1) AND accept = false")
+    List<Friend> SelectRelationMyFriends (Long idApplicant, Long idReceiver);
 }
