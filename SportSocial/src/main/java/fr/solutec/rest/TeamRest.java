7d04cba328cdf78dca75bdadb3462314f6f6408e
HugoLePetitDev <hugopineau98@gmail.com>
2023-01-02 16:09:51 +0100

    Quitter groupe

--- SportSocial/src/main/java/fr/solutec/rest/TeamRest.java
+++ SportSocial/src/main/java/fr/solutec/rest/TeamRest.java
@@ -6,12 +6,14 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import fr.solutec.entities.Event;
 import fr.solutec.entities.Friend;
 import fr.solutec.entities.Message;
 import fr.solutec.entities.Messagerie;
@@ -60,6 +62,16 @@ public class TeamRest {
 
         return messageRepo.save(m);
     }
+	
+    @DeleteMapping("team/quit/{idTeam}/{idUser}") //Quitter un groupe 
+    public Team quitTeam(@PathVariable Long idTeam, @PathVariable Long idUser) {
+    	Team team = teamRepo.findById(idTeam).get();
+    	team.getMembres().removeIf(p-> p.getIdUser()==idUser);
+    	return teamRepo.save(team);
+    	
+    }
+
+       
 	
 	
 
