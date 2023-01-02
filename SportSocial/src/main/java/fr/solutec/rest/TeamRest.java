8d6840bb0e88fa574df8dbc3ce7abd3f6509ad74
HugoLePetitDev <hugopineau98@gmail.com>
2023-01-02 14:59:40 +0100

    Message de groupe

--- SportSocial/src/main/java/fr/solutec/rest/TeamRest.java
+++ SportSocial/src/main/java/fr/solutec/rest/TeamRest.java
@@ -8,17 +8,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import fr.solutec.entities.Friend;
+import fr.solutec.entities.Message;
+import fr.solutec.entities.Messagerie;
 import fr.solutec.entities.Team;
 import fr.solutec.entities.User;
+import fr.solutec.repository.MessageRepository;
 import fr.solutec.repository.TeamRepository;
+import fr.solutec.repository.UserRepository;
 
 @RestController @CrossOrigin("*")
 public class TeamRest {
 	@Autowired
 	private TeamRepository teamRepo;
+	@Autowired 
+	private MessageRepository messageRepo;
+	@Autowired
+	private UserRepository userRepo;
+	
 	
 	@GetMapping("team/{idUser}")
 	private List<Team> MyTeams(@PathVariable Long idUser){
@@ -35,6 +46,20 @@ public class TeamRest {
 		return teamRepo.getMyTeamMessage(idTeam);
 		
 	}
+	
+	@PostMapping("team/envoyer/{idTeam}/{idExp}")
+    public Message sendTeamMessageById(@PathVariable Long idTeam, @PathVariable Long idExp, @RequestBody Message message) {
+
+        String contenu= message.getContentMessage();
+        Optional<User>uexp=userRepo.findByIdUser(idExp);
+        Message m = new Message (null,null,contenu,uexp.get());
+        Message msave = messageRepo.save(m);
+
+        Optional<Team>team = teamRepo.findById(idTeam);
+        team.get().getConversation().add(msave);
+
+        return messageRepo.save(m);
+    }
 	
 	
 
