248a1d759efd435297365e6893a8a9a1e9dd3b2e
LucyDdt <dolucy@etudiant-esic.fr>
2023-01-05 19:13:21 +0100

    essai query club qui marche pas + ajout de personnes dans un club

--- SportSocial/src/main/java/fr/solutec/repository/ClubRepository.java
+++ SportSocial/src/main/java/fr/solutec/repository/ClubRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import fr.solutec.entities.Club;
+import fr.solutec.entities.User;
 
 public interface ClubRepository extends CrudRepository<Club, Long>{
 	
@@ -14,4 +15,10 @@ public interface ClubRepository extends CrudRepository<Club, Long>{
 	
 	@Query("SELECT c FROM Club c WHERE c.idClub NOT IN (SELECT c.idClub FROM Club c INNER JOIN c.membres cm WHERE cm.idUser = ?1)ORDER BY c.titleClub ASC ")
 	List<Club> getOtherClubs(Long idUser);
+
+
+	
+
+
+
 }
