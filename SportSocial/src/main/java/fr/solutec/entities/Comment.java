c33575792d5cfc4ed2eca4f09b013706b880af4e
LucyDdt <dolucy@etudiant-esic.fr>
2023-01-16 17:04:37 +0100

    Ajout de commentaires

--- SportSocial/src/main/java/fr/solutec/entities/Comment.java
+++ SportSocial/src/main/java/fr/solutec/entities/Comment.java
@@ -1,12 +1,14 @@
 package fr.solutec.entities;
 
 import java.sql.Timestamp;
-
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 
 import org.hibernate.annotations.CreationTimestamp;
@@ -32,5 +34,6 @@ public class Comment {
 	private String contentComment;
 	@ManyToOne
 	private User createurComment;
-	
+	@ManyToMany
+	private List<Comment> comments = new ArrayList<Comment>();
 }
