f12a0e1af3d304d25070120f122768318c4dfb93
LucyDdt <dolucy@etudiant-esic.fr>
2023-01-18 18:02:39 +0100

    ajout API créer post et créer messagesPost

--- SportSocial/src/main/java/fr/solutec/rest/CommentRest.java
+++ SportSocial/src/main/java/fr/solutec/rest/CommentRest.java
@@ -1,15 +1,22 @@
 package fr.solutec.rest;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import fr.solutec.entities.Comment;
+import fr.solutec.entities.Post;
+import fr.solutec.entities.User;
 import fr.solutec.repository.CommentRepository;
+import fr.solutec.repository.PostRepository;
+import fr.solutec.repository.UserRepository;
 
 @RestController
 @CrossOrigin("*")
@@ -17,6 +24,12 @@ public class CommentRest {
 
 	@Autowired
 	private CommentRepository commentRepo;
+	
+	@Autowired
+	private UserRepository userRepo;
+	
+	@Autowired
+	private PostRepository postRepo;
 	
 	@GetMapping("club/posts/comments/{idPost}")
 	public Iterable<Comment> getCommentsInPost(@PathVariable Long idPost){
@@ -46,4 +59,12 @@ public class CommentRest {
 		return Comments;
 	}
 	
+	@PostMapping("/post/comment/{idUser}/{idPost}")
+	public Comment createCommentInPost(@PathVariable Long idUser, @PathVariable Long idPost, @RequestBody String contenu) {
+		Optional<User> user = userRepo.findById(idUser);
+		Optional<Post> post = postRepo.findById(idPost);
+		Comment c = new Comment(null, null, contenu, user.get(), null, null, 0, 0);
+		post.get().getCommentsPost().add(c);
+		return commentRepo.save(c);
+	}
 }
--- SportSocial/src/main/java/fr/solutec/rest/PostRest.java
+++ SportSocial/src/main/java/fr/solutec/rest/PostRest.java
@@ -9,11 +9,15 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import fr.solutec.entities.Club;
 import fr.solutec.entities.Comment;
 import fr.solutec.entities.Post;
 import fr.solutec.entities.User;
+import fr.solutec.repository.ClubRepository;
 import fr.solutec.repository.CommentRepository;
 import fr.solutec.repository.PostRepository;
 import fr.solutec.repository.UserRepository;
@@ -30,6 +34,9 @@ public class PostRest {
 	
 	@Autowired
 	private UserRepository userRepo;
+	
+	@Autowired
+	private ClubRepository clubRepo;
 	
 	
 	@GetMapping("club/posts/{idClub}")
@@ -77,6 +84,14 @@ public class PostRest {
 		Optional<Post> p = postRepo.findById(idPost);
 		p.get().getLikePost().removeIf(u -> u.getIdUser()==idUser);
 		return postRepo.save(p.get());
+	}
+	
+	@PostMapping("club/posts/add/{idUser}/{idClub}")
+	public Post createClubPost(@PathVariable Long idUser, @PathVariable Long idClub, @RequestBody String contenu) {
+		Optional<User> user = userRepo.findById(idUser);
+		Optional<Club> club = clubRepo.findById(idClub);
+		Post p = new Post(null, null, contenu, null, user.get(), club.get(), null, null, 0, 0);
+		return postRepo.save(p);
 	}
 	
 }
