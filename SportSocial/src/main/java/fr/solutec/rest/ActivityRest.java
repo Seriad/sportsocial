64b25db3f4dff1af22eb7e387e3ccba1a6daec15
HugoLePetitDev <hugopineau98@gmail.com>
2023-01-19 12:17:52 +0100

    Merge branch 'master' of https://github.com/Seriad/sportsocial.git

--- SportSocial/src/main/java/fr/solutec/rest/ActivityRest.java
+++ SportSocial/src/main/java/fr/solutec/rest/ActivityRest.java
@@ -1,13 +1,20 @@
 package fr.solutec.rest;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import fr.solutec.entities.Activity;
+import fr.solutec.entities.User;
+import fr.solutec.entities.UserActivity;
 import fr.solutec.repository.ActivityRepository;
+import fr.solutec.repository.UserActivityRepository;
+import fr.solutec.repository.UserRepository;
 
 
 @RestController
@@ -16,13 +23,21 @@ public class ActivityRest {
 	
 	@Autowired
 	private ActivityRepository activityRepo;
+	@Autowired
+	private UserActivityRepository userActivityRepo;
+	@Autowired
+	private UserRepository userRepo;
 	
 	
-	
-	
-	@PostMapping("schedule")
-	public Activity saveActivity(@RequestBody Activity a) {
-		return activityRepo.save(a);
+	@PostMapping("schedule/{idUser}")
+	public Activity saveActivity(@RequestBody Activity a, @PathVariable Long idUser) {
+		User user = userRepo.findByIdUser(idUser).get();
+		activityRepo.save(a);
+		
+		UserActivity act = new UserActivity(user,a);
+		userActivityRepo.save(act);
+
+		return a;
 	}
 	
 
