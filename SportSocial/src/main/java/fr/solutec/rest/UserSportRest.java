db18c27c2f9262c6a6af422e5df874b9049b2151
HugoLePetitDev <hugopineau98@gmail.com>
2023-01-25 15:02:36 +0100

    MAJ ranking (add score)

--- SportSocial/src/main/java/fr/solutec/rest/UserSportRest.java
+++ SportSocial/src/main/java/fr/solutec/rest/UserSportRest.java
@@ -7,9 +7,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import fr.solutec.entities.Sport;
+import fr.solutec.entities.Training;
+import fr.solutec.entities.User;
 import fr.solutec.entities.UserSport;
+import fr.solutec.repository.UserRepository;
 import fr.solutec.repository.UserSportRepository;
 
 @RestController @CrossOrigin("*")
@@ -17,6 +23,8 @@ public class UserSportRest {
 
 	@Autowired
 	private UserSportRepository userSportRepo;
+	@Autowired
+	private UserRepository userRepo;
 	
 	@GetMapping("classement/{nameSport}/asc")
 	public List<UserSport> rankingBySportasc(@PathVariable String nameSport){
@@ -31,4 +39,23 @@ public class UserSportRest {
 		
 		return userSportRepo.findByUserIdUser(iduser);
 	}
+	
+	@PostMapping("classement/add/{idUser}")
+		public UserSport addRanking(@PathVariable Long idUser, @RequestBody UserSport usersport) {
+		
+		User user = userRepo.findUserById(idUser);
+		
+		int newScore = usersport.getScore();
+		Sport addSport = usersport.getSport();
+		
+		UserSport newRanking = new UserSport();
+		
+		newRanking.setScore(newScore);
+		newRanking.setSport(addSport);
+		newRanking.setUser(user);
+		
+		return userSportRepo.save(usersport);
+	}
+
+	
 }
