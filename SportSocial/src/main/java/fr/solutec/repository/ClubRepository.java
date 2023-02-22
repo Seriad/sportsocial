3822d4c75814176fa8652710fb21ccf868d10015
LucyDdt <dolucy@etudiant-esic.fr>
2023-02-22 14:27:23 +0100

    API admin et membres corrigée

--- SportSocial/src/main/java/fr/solutec/repository/ClubRepository.java
+++ SportSocial/src/main/java/fr/solutec/repository/ClubRepository.java
@@ -22,10 +22,10 @@ public interface ClubRepository extends CrudRepository<Club, Long>{
 	@Query("SELECT c FROM Club c INNER JOIN c.sportClub cs WHERE c.idClub NOT IN (SELECT c.idClub FROM Club c INNER JOIN c.membres cm WHERE cm.idUser = ?1) AND (cs.nameSport = ?2) ORDER BY c.titleClub ASC ")
 	List<Club> filterOtherClubs(Long idUser, String nameSport);
 
-	@Query("SELECT ca FROM Club c INNER JOIN c.admin ca")
+	@Query("SELECT ca FROM Club c INNER JOIN c.admin ca WHERE c.idClub = ?1")
 	List<User> getAdmin(Long idClub);
 
-	@Query("SELECT cm FROM Club c INNER JOIN c.membres cm WHERE cm.idUser NOT IN (SELECT ca.idUser FROM Club c INNER JOIN c.admin ca)")
+	@Query("SELECT cm FROM Club c INNER JOIN c.membres cm WHERE cm.idUser NOT IN (SELECT ca.idUser FROM Club c INNER JOIN c.admin ca WHERE c.idClub = ?1) AND c.idClub = ?1")
 	List<User> getMembresWithoutAdmin(Long idClub);
 
 }
