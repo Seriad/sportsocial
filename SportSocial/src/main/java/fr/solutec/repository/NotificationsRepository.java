b4076a6fcfb46c2ad80e0a0cf724b2c7a795b71c
LucyDdt <dolucy@etudiant-esic.fr>
2023-03-01 17:43:42 +0100

    APIs notifs ajout + suppression admin, envoi message, récup notifs

--- SportSocial/src/main/java/fr/solutec/repository/NotificationsRepository.java
+++ SportSocial/src/main/java/fr/solutec/repository/NotificationsRepository.java
@@ -2,10 +2,15 @@ package fr.solutec.repository;
 
 
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import fr.solutec.entities.Notifications;
 
 public interface NotificationsRepository extends CrudRepository<Notifications, Long>{
 
+	List<Notifications> findByDestinataireIdUser(Long idUser);
+	
+	
 }
--- SportSocial/src/main/java/fr/solutec/rest/NotificationsRest.java
+++ SportSocial/src/main/java/fr/solutec/rest/NotificationsRest.java
@@ -1,13 +1,16 @@
 package fr.solutec.rest;
 
 
+import java.util.List;
 import java.util.Optional;
 
-import javax.persistence.ManyToOne;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import fr.solutec.entities.Club;
 import fr.solutec.entities.Notifications;
@@ -16,6 +19,9 @@ import fr.solutec.repository.ClubRepository;
 import fr.solutec.repository.NotificationsRepository;
 import fr.solutec.repository.UserRepository;
 
+
+@RestController
+@CrossOrigin("*")
 public class NotificationsRest {
 
 	@Autowired
@@ -32,8 +38,33 @@ public class NotificationsRest {
 		Optional<User> admin = userRepo.findById(idAdmin);
 		Optional<User> user = userRepo.findById(idUser);
 		Optional<Club> club = clubRepo.findById(idClub);
-		
-		Notifications notif = new Notifications(null,admin.get().getLoginUser()+" vous a ajouté comme administrateur du club " + club.get().getTitleClub(), admin.get(), user.get(), false);
+		String contenu = admin.get().getLoginUser()+" vous a ajouté comme administrateur du club " + club.get().getTitleClub();
+		Notifications notif = new Notifications(null,contenu, admin.get(), user.get(), false);
 		return notifRepo.save(notif);
 	}
+	
+	@PostMapping("notification/adminToUser/delete/{idUser}/{idClub}")
+	public Notifications notifDeletedAdmin(@PathVariable Long idUser, @PathVariable Long idClub) {
+		Optional<User> user = userRepo.findById(idUser);
+		Optional<Club> club = clubRepo.findById(idClub);
+		String contenu = "Vous avez été destitué de votre rôle d'administrateur du club " + club.get().getTitleClub();
+		Notifications notif = new Notifications(null,contenu, null, user.get(), false);
+		return notifRepo.save(notif);
+	}
+	
+	@PostMapping("notification/userToUser/answer/{idSender}/{idReceiver}")
+	public Notifications notifAnswerUser(@PathVariable Long idSender, @PathVariable Long idReceiver) {
+		Optional<User> sender = userRepo.findById(idSender);
+		Optional<User> receiver = userRepo.findById(idReceiver);
+		String contenu = sender.get().getLoginUser()+" a répondu à l'un de vos commentaires";
+		Notifications notif = new Notifications(null,contenu, sender.get(), receiver.get(), false);
+		return notifRepo.save(notif);
+	}
+	
+	@GetMapping("notifications/liste/{idUser}")
+	public List<Notifications> getNotifications(@PathVariable Long idUser){
+		List<Notifications> notifs = notifRepo.findByDestinataireIdUser(idUser);
+		return notifs;
+	}
+	
 }
